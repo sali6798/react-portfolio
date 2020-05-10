7585de79b7db6762f7fb28f610d667fc2ba465ab
sali6798 <shaidee@alingcastre.net>
2020-05-10 11:32:17 -0700

    server config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 // *** Dependencies
 // =============================================================
 const express = require("express");
-const path = require("path");
+const routes = require("./routes");
 require("dotenv").config();
 
 // Sets up the Express App
@@ -18,16 +18,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
 // Routes
 // =============================================================
-app.get('*', (req, res) => {
-    res.json("hello");
-});
+app.use(routes);
 
 
 // Start the API server
 app.listen(PORT, function () {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`API Server now listening on PORT ${PORT}!`);
 });
