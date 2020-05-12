595cbdc43aac53ca8ad6815de47f7b64d1a7aef3
sali6798 <shaidee@alingcastre.net>
2020-05-11 17:27:17 -0700

    changed icons to saved icons

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -8,15 +8,15 @@ function Contact() {
     }, []);
 
     return (
-        <div>
+        <div className="Contact">
             <h2>Contact</h2>
             <hr />
             <div className="row">
                 <div className="col-md-6 col-lg-5 contact-info">
-                    <p><i className="fas fa-phone-alt"></i>(206) 799-9002</p>
-                    <p><i className="fas fa-envelope"></i>shaidee@alingcastre.net</p>
-                    <p><i className="fab fa-linkedin"></i><a href="https://www.linkedin.com/in/shaidee-alingcastre" target="_blank" rel="noreferrer noopener">Shaidee Alingcastre</a></p>
-                    <p><i className="fab fa-github"></i><a href="https://github.com/sali6798" target="_blank" rel="noreferrer noopener">@sali6798</a></p>
+                    <p><img src="/assets/phone-alt-solid.svg" alt="phone icon"></img>(206) 799-9002</p>
+                    <p><img src="/assets/envelope-solid.svg" alt="envelope icon"></img>shaidee@alingcastre.net</p>
+                    <p><img src="/assets/linkedin-brands.svg" alt="linkedin icon"></img><a href="https://www.linkedin.com/in/shaidee-alingcastre" target="_blank" rel="noreferrer noopener">Shaidee Alingcastre</a></p>
+                    <p><img src="/assets/github-brands.svg" alt="github icon"></img><a href="https://github.com/sali6798" target="_blank" rel="noreferrer noopener">@sali6798</a></p>
                 </div>
                 <div className="col-md-6 col-lg-7">
                     <Form />
